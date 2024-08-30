3da6ec087d458da7834cd1af37da13fb1d26b0d4
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2024-08-30 14:38:05 +0545

    corrected all codes

--- src/Prisma/prisma.service.ts
+++ src/Prisma/prisma.service.ts
@@ -3,6 +3,8 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
+
+// prisma service for working on database cls
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
     super({
